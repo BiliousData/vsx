fa683d308ef57a316ce4595fe78024d2ecc173a8
BiliousData <goofypuppydog@outlook.com>
2021-10-12 16:26:31 -0500

    rework vtown

--- src/stage/vtown.c
+++ src/stage/vtown.c
@@ -1,26 +1,21 @@
-/*
-  This Source Code Form is subject to the terms of the Mozilla Public
-  License, v. 2.0. If a copy of the MPL was not distributed with this
-  file, You can obtain one at http://mozilla.org/MPL/2.0/.
-*/
-
 #include "vtown.h"
 
 #include "../archive.h"
 #include "../mem.h"
 #include "../stage.h"
 
-// /v/ town background structure
+//Week 1 background structure
 typedef struct
 {
 	//Stage background base structure
 	StageBack back;
 	
 	//Textures
-	Gfx_Tex tex_back0; //Uhh everything
+	Gfx_Tex tex_back0; //Stage and back
+	Gfx_Tex tex_back1; //Sky
 } Back_Vtown;
 
-// /v/ town 1 background functions
+//Week 1 background functions
 void Back_Vtown_DrawBG(StageBack *back)
 {
 	Back_Vtown *this = (Back_Vtown*)back;
@@ -31,13 +26,12 @@ void Back_Vtown_DrawBG(StageBack *back)
 	fx = stage.camera.x;
 	fy = stage.camera.y;
 	
-	//Draw window light
-	RECT stage_src = {0, 128, 256, 128};
+	RECT stage_src = {0, 0, 256, 81};
 	RECT_FIXED stage_dst = {
-		FIXED_DEC(-130,1) - fx,
-		FIXED_DEC(44,1) - fy,
-		FIXED_DEC(350,1),
-		FIXED_DEC(148,1)
+		FIXED_DEC(-255,1) - FIXED_DEC(SCREEN_WIDEOADD,2) - fx,
+		FIXED_DEC(30,1) - fy,
+		FIXED_DEC(450,1) + FIXED_DEC(SCREEN_WIDEOADD,1),
+		FIXED_DEC(115,1)
 	};
 	
 	Stage_DrawTex(&this->tex_back0, &stage_src, &stage_dst, stage.camera.bzoom);
@@ -46,19 +40,23 @@ void Back_Vtown_DrawBG(StageBack *back)
 	fx = stage.camera.x * 2 / 3;
 	fy = stage.camera.y * 2 / 3;
 	
-	RECT backl_src = {0, 59, 121, 105};
+	RECT backl_src = {0, 59, 256, 197};
 	RECT_FIXED backl_dst = {
-		FIXED_DEC(-190,1) - fx,
-		FIXED_DEC(-100,1) - fy,
-		FIXED_DEC(121,1),
-		FIXED_DEC(105,1)
+		FIXED_DEC(-250,1) - fx,
+		FIXED_DEC(-230,1) - fy,
+		FIXED_DEC(450,1),
+		FIXED_DEC(350,1)
 	};
-	RECT backr_src = {121, 59, 136, 120};
+
+	fx = stage.camera.x * 1 / 3;
+	fy = stage.camera.y * 1 / 3;
+
+	RECT backr_src = {0, 0, 256, 173};
 	RECT_FIXED backr_dst = {
-		FIXED_DEC(60,1) - fx,
-		FIXED_DEC(-110,1) - fy,
-		FIXED_DEC(136,1),
-		FIXED_DEC(120,1)
+		FIXED_DEC(-250,1) - fx,
+		FIXED_DEC(-200,1) - fy,
+		FIXED_DEC(450,1),
+		FIXED_DEC(350,1)
 	};
 	RECT backf_src = {0, 59, 1, 1};
 	RECT backf_dst = {
@@ -69,7 +67,7 @@ void Back_Vtown_DrawBG(StageBack *back)
 	};
 	
 	Stage_DrawTex(&this->tex_back0, &backl_src, &backl_dst, stage.camera.bzoom);
-	Stage_DrawTex(&this->tex_back0, &backr_src, &backr_dst, stage.camera.bzoom);
+	Stage_DrawTex(&this->tex_back1, &backr_src, &backr_dst, stage.camera.bzoom);
 	Gfx_DrawTex(&this->tex_back0, &backf_src, &backf_dst);
 }
 
@@ -97,6 +95,7 @@ StageBack *Back_Vtown_New(void)
 	//Load background textures
 	IO_Data arc_back = IO_Read("\\WEEK8\\BACK.ARC;1");
 	Gfx_LoadTex(&this->tex_back0, Archive_Find(arc_back, "back0.tim"), 0);
+	Gfx_LoadTex(&this->tex_back1, Archive_Find(arc_back, "back1.tim"), 0);
 	Mem_Free(arc_back);
 	
 	return (StageBack*)this;
