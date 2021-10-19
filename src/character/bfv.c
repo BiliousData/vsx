dff4c5183eca1325f1df3be78261e03e47b96b4f
BiliousData <goofypuppydog@outlook.com>
2021-10-19 06:46:53 -0500

    bf/v/ game over

--- src/character/bfv.c
+++ src/character/bfv.c
@@ -12,44 +12,25 @@
 #include "../random.h"
 #include "../main.h"
 
-//Boyfriend /v/ unused skull fragments
-static SkullFragment char_bfv_skull[15] = {
-	{ 1 * 8, -87 * 8, -13, -13},
-	{ 9 * 8, -88 * 8,   5, -22},
-	{18 * 8, -87 * 8,   9, -22},
-	{26 * 8, -85 * 8,  13, -13},
-	
-	{-3 * 8, -82 * 8, -13, -11},
-	{ 8 * 8, -85 * 8,  -9, -15},
-	{20 * 8, -82 * 8,   9, -15},
-	{30 * 8, -79 * 8,  13, -11},
-	
-	{-1 * 8, -74 * 8, -13, -5},
-	{ 8 * 8, -77 * 8,  -9, -9},
-	{19 * 8, -75 * 8,   9, -9},
-	{26 * 8, -74 * 8,  13, -5},
-	
-	{ 5 * 8, -73 * 8, -5, -3},
-	{14 * 8, -76 * 8,  9, -6},
-	{26 * 8, -67 * 8, 15, -3},
-};
 
 //Boyfriend /v/ player types
 enum
 {
 	BFV_ArcMain_Hit,  //Left Down
 	BFV_ArcMain_Miss, //Left Down
+	BFV_ArcMain_Ban0, //BREAK
+	BFV_ArcMain_Ban1,
+	BFV_ArcMain_Ban2,
 	
 	BFV_ArcMain_Max,
 };
 
 enum
 {
-	BFV_ArcDead_Dead1, //Mic Drop
-	BFV_ArcDead_Dead2, //Twitch
-	BFV_ArcDead_Retry, //Retry prompt
+	BFV_ArcBan_Ban1, //Mic Drop
+	BFV_ArcBan_Ban2, //Twitch
 	
-	BFV_ArcDead_Max,
+	BFV_ArcBan_Max,
 };
 
 #define BFV_Arc_Max BFV_ArcMain_Max
@@ -68,8 +49,6 @@ typedef struct
 	
 	u8 retry_bump;
 	
-	SkullFragment skull[COUNT_OF(char_bfv_skull)];
-	u8 skull_scale;
 } Char_BFV;
 
 //Boyfriend /v/ player definitions
@@ -99,6 +78,15 @@ static const CharFrame char_bfv_frame[] = {
 	{BFV_ArcMain_Miss, {  0,  62,  56,  64}, { 45, 92}}, //18 right miss 1
 	{BFV_ArcMain_Miss, { 57,  61,  57,  64}, { 45, 93}}, //19 right miss 2
 
+	{BFV_ArcMain_Ban0,  { 0,  0,  161, 129}, {53, 92}}, //20
+	{BFV_ArcMain_Ban0,  { 0,  127,  161, 129}, {53, 92}}, //21
+
+	{BFV_ArcMain_Ban1,  { 0,  0,  161, 129}, {53, 92}},//22
+	{BFV_ArcMain_Ban1,  { 0,  127,  161, 129}, {53, 92}},//23
+
+	{BFV_ArcMain_Ban2,  { 0,  0,  161, 129}, {53, 92}},//24
+	{BFV_ArcMain_Ban2,  { 0,  127,  161, 129}, {53, 92}},//25
+
 };
 
 static const Animation char_bfv_anim[PlayerAnim_Max] = {
@@ -114,13 +102,13 @@ static const Animation char_bfv_anim[PlayerAnim_Max] = {
 	{2, (const u8[]){ASCR_CHGANI, CharAnim_Idle}},         //PlayerAnim_Peace
 	{2, (const u8[]){ASCR_CHGANI, CharAnim_Idle}},       //PlayerAnim_Sweat
 	
-	{2, (const u8[]){ 0,  1,  2,  3, 4, ASCR_BACK, 1}}, //CharAnim_Idle
-	{2, (const u8[]){ 0,  1,  2,  3, 4, ASCR_BACK, 1}}, //CharAnim_Idle
-	{2, (const u8[]){ 0,  1,  2,  3, 4, ASCR_BACK, 1}}, //CharAnim_Idle
-	{2, (const u8[]){ 0,  1,  2,  3, 4, ASCR_BACK, 1}}, //CharAnim_Idle
-	{2, (const u8[]){ 0,  1,  2,  3, 4, ASCR_BACK, 1}}, //CharAnim_Idle
-	{2, (const u8[]){ 0,  1,  2,  3, 4, ASCR_BACK, 1}}, //CharAnim_Idle
-	{2, (const u8[]){ 0,  1,  2,  3, 4, ASCR_BACK, 1}}, //CharAnim_Idle
+	{3, (const u8[]){ 20, 21, ASCR_CHGANI, PlayerAnim_Dead1}}, //CharAnim_Idle
+	{3, (const u8[]){ 22, 23, ASCR_CHGANI, PlayerAnim_Dead2}}, //CharAnim_Idle
+	{20, (const u8[]){ 24, ASCR_CHGANI, PlayerAnim_Dead3}}, //CharAnim_Idle
+	{1, (const u8[]){ 25, ASCR_CHGANI, PlayerAnim_Dead3}}, //CharAnim_Idle
+	{2, (const u8[]){ ASCR_CHGANI, PlayerAnim_Dead3}}, //CharAnim_Idle
+	{2, (const u8[]){ ASCR_CHGANI, PlayerAnim_Dead3}}, //CharAnim_Idle
+	{2, (const u8[]){ ASCR_CHGANI, PlayerAnim_Dead3}}, //CharAnim_Idle
 };
 
 //Boyfriend /v/ player functions
@@ -180,105 +168,6 @@ void Char_BFV_Tick(Character *character)
 		}
 	}
 	
-	//Retry screen
-	if (character->animatable.anim >= PlayerAnim_Dead3)
-	{
-		//Tick skull fragments
-		if (this->skull_scale)
-		{
-			SkullFragment *frag = this->skull;
-			for (size_t i = 0; i < COUNT_OF_MEMBER(Char_BFV, skull); i++, frag++)
-			{
-				//Draw fragment
-				RECT frag_src = {
-					(i & 1) ? 112 : 96,
-					(i >> 1) << 4,
-					16,
-					16
-				};
-				fixed_t skull_dim = (FIXED_DEC(16,1) * this->skull_scale) >> 6;
-				fixed_t skull_rad = skull_dim >> 1;
-				RECT_FIXED frag_dst = {
-					character->x + (((fixed_t)frag->x << FIXED_SHIFT) >> 3) - skull_rad - stage.camera.x,
-					character->y + (((fixed_t)frag->y << FIXED_SHIFT) >> 3) - skull_rad - stage.camera.y,
-					skull_dim,
-					skull_dim,
-				};
-				Stage_DrawTex(&this->tex_retry, &frag_src, &frag_dst, FIXED_MUL(stage.camera.zoom, stage.bump));
-				
-				//Move fragment
-				frag->x += frag->xsp;
-				frag->y += ++frag->ysp;
-			}
-			
-			//Decrease scale
-			this->skull_scale--;
-		}
-		
-		//Draw input options
-		u8 input_scale = 16 - this->skull_scale;
-		if (input_scale > 16)
-			input_scale = 0;
-		
-		RECT button_src = {
-			 0, 96,
-			16, 16
-		};
-		RECT_FIXED button_dst = {
-			character->x - FIXED_DEC(32,1) - stage.camera.x,
-			character->y - FIXED_DEC(88,1) - stage.camera.y,
-			(FIXED_DEC(16,1) * input_scale) >> 4,
-			FIXED_DEC(16,1),
-		};
-		
-		//Cross - Retry
-		Stage_DrawTex(&this->tex_retry, &button_src, &button_dst, FIXED_MUL(stage.camera.zoom, stage.bump));
-		
-		//Circle - Blueball
-		button_src.x = 16;
-		button_dst.y += FIXED_DEC(56,1);
-		Stage_DrawTex(&this->tex_retry, &button_src, &button_dst, FIXED_MUL(stage.camera.zoom, stage.bump));
-		
-		//Draw 'RETRY'
-		u8 retry_frame;
-		
-		if (character->animatable.anim == PlayerAnim_Dead6)
-		{
-			//Selected retry
-			retry_frame = 2 - (this->retry_bump >> 3);
-			if (retry_frame >= 3)
-				retry_frame = 0;
-			if (this->retry_bump & 2)
-				retry_frame += 3;
-			
-			if (++this->retry_bump == 0xFF)
-				this->retry_bump = 0xFD;
-		}
-		else
-		{
-			//Idle
-			retry_frame = 1 +  (this->retry_bump >> 2);
-			if (retry_frame >= 3)
-				retry_frame = 0;
-			
-			if (++this->retry_bump >= 55)
-				this->retry_bump = 0;
-		}
-		
-		RECT retry_src = {
-			(retry_frame & 1) ? 48 : 0,
-			(retry_frame >> 1) << 5,
-			48,
-			32
-		};
-		RECT_FIXED retry_dst = {
-			character->x -  FIXED_DEC(7,1) - stage.camera.x,
-			character->y - FIXED_DEC(92,1) - stage.camera.y,
-			FIXED_DEC(48,1),
-			FIXED_DEC(32,1),
-		};
-		Stage_DrawTex(&this->tex_retry, &retry_src, &retry_dst, FIXED_MUL(stage.camera.zoom, stage.bump));
-	}
 	
 	//Animate and draw character
 	Animatable_Animate(&character->animatable, (void*)this, Char_BFV_SetFrame);
@@ -288,6 +177,17 @@ void Char_BFV_Tick(Character *character)
 void Char_BFV_SetAnim(Character *character, u8 anim)
 {
 	Char_BFV *this = (Char_BFV*)character;
+
+	//Perform animation checks
+	switch (anim)
+	{
+		case PlayerAnim_Dead0:
+			//Begin reading dead.arc and adjust focus
+			character->focus_x = FIXED_DEC(0,1);
+			character->focus_y = FIXED_DEC(-40,1);
+			character->focus_zoom = FIXED_DEC(125,100);
+			break;
+	}
 	
 	//Set animation
 	Animatable_SetAnim(&character->animatable, anim);
@@ -336,6 +236,9 @@ Character *Char_BFV_New(fixed_t x, fixed_t y)
 	const char **pathp = (const char *[]){
 		"hit.tim",  //Everything except miss lol
 		"miss.tim", //BFV_ArcMain_Miss0
+		"ban0.tim",
+		"ban1.tim",
+		"ban2.tim",
 		NULL
 	};
 	IO_Data *arc_ptr = this->arc_ptr;
@@ -348,17 +251,6 @@ Character *Char_BFV_New(fixed_t x, fixed_t y)
 	//Initialize player state
 	this->retry_bump = 0;
 	
-	//Copy skull fragments
-	memcpy(this->skull, char_bfv_skull, sizeof(char_bfv_skull));
-	this->skull_scale = 64;
-	
-	SkullFragment *frag = this->skull;
-	for (size_t i = 0; i < COUNT_OF_MEMBER(Char_BFV, skull); i++, frag++)
-	{
-		//Randomize trajectory
-		frag->xsp += RandomRange(-4, 4);
-		frag->ysp += RandomRange(-2, 2);
-	}
 	
 	return (Character*)this;
 }
