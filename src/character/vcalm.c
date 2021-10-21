fdc1ebc98908837acad88c6674b17df714b9d289
BiliousData <goofypuppydog@outlook.com>
2021-10-20 21:08:11 -0500

    add vcalm icons

--- src/character/vcalm.c
+++ src/character/vcalm.c
@@ -202,7 +202,7 @@ Character *Char_Vcalm_New(fixed_t x, fixed_t y)
 	//Set character information
 	this->character.spec = 0;
 	
-	this->character.health_i = 12;
+	this->character.health_i = 13;
 	
 	this->character.focus_x = FIXED_DEC(65,1);
 	this->character.focus_y = FIXED_DEC(-125,1);
